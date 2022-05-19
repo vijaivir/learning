61fee44500cb1bb71b0fb61bdb0ed01837b5fcfd
vijaivir <91633223+vijaivir@users.noreply.github.com>
2022-05-19 12:20:42 -0700

    Update index.js

--- blackjack/index.js
+++ blackjack/index.js
@@ -1,3 +1,25 @@
-let firstCard = 5;
-let secondCard = 10;
-let sum = firstCard + secondCard;
+let firstCard = 21
+let secondCard = 0
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el");
+let sumEl = document.querySelector("#sum-el");
+
+function startGame(){
+    sumEl.textContent += " " + sum;
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message;
+}
+
+sumEl.textContent = "Sum: "
+
