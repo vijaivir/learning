3c9bbe31034ca70814e446463305f2236b9f0ab3
vijaivir <91633223+vijaivir@users.noreply.github.com>
2022-05-18 19:44:11 -0700

    Update index.js

--- unit-converter/index.js
+++ unit-converter/index.js
@@ -5,10 +5,19 @@ const textBox2 = document.getElementById("volume");
 const textBox3 = document.getElementById("mass");
 
 function convert(){
-    num = result.value;
+    let num = result.value;
     heading.textContent = num;
-    feet = num * 3.281;
-    meters = num/3.281;
+
+    let feet = num * 3.281;
+    let meters = num * 0.305;
     textBox1.textContent = num + " meters = " + feet + " feet | " + num + " feet = " + meters + " meters";
+
+    let gallons = num * 0.264;
+    let litres = num * 4.546;
+    textBox2.textContent = num + " litres = " + gallons + " gallons | " + num + " gallons = " + litres + " litres";
+   
+    let kilos = num * 0.454;
+    let pounds = num * 2.205;
+    textBox3.textContent = num + " kilos = " + pounds + " lbs | " + num + " lbs = " + kilos + " kg";
 
 }
