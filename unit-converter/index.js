99615c5516c605f7e45ca81f09d3407d6f98daf6
vijaivir <91633223+vijaivir@users.noreply.github.com>
2022-05-19 10:38:44 -0700

    Update index.js

--- unit-converter/index.js
+++ unit-converter/index.js
@@ -1,23 +1,31 @@
-const result = document.getElementById("user-input");
-const heading = document.getElementById("input");
-const textBox1 = document.getElementById("length");
-const textBox2 = document.getElementById("volume");
-const textBox3 = document.getElementById("mass");
+let heading = document.getElementById("input");
+let textBox1 = document.getElementById("length");
+let textBox2 = document.getElementById("volume");
+let textBox3 = document.getElementById("mass");
 
 function convert(){
-    let num = result.value;
-    heading.textContent = num;
 
-    let feet = num * 3.281;
-    let meters = num * 0.305;
+    //let result = document.getElementById("user-input");
+    //let num = result.value;
+    let num = 20;
+    heading.innerHTML = num;
+
+    let feet = (num * 3.28084).toFixed(3);
+    let meters = (num /3.28084).toFixed(3);
     textBox1.textContent = num + " meters = " + feet + " feet | " + num + " feet = " + meters + " meters";
 
-    let gallons = num * 0.264;
-    let litres = num * 4.546;
+    let litres = (num * 3.785411784).toFixed(3);
+    let gallons = (num / 3.785411784).toFixed(3);
     textBox2.textContent = num + " litres = " + gallons + " gallons | " + num + " gallons = " + litres + " litres";
    
-    let kilos = num * 0.454;
-    let pounds = num * 2.205;
+    let kilos = (num * 0.454).toFixed(3);
+    let pounds = (num * 2.205).toFixed(3);
     textBox3.textContent = num + " kilos = " + pounds + " lbs | " + num + " lbs = " + kilos + " kg";
 
 }
+
+/* <form id="form" onsubmit="convert()" novalidate >
+<input type="text" name="value" id="user-input" placeholder="Enter Number For Conversion...">
+</form>
+*/
+convert() 
