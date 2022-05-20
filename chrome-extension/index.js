d2a67a8bb39f0e319798df3efa684d20a7bb512c
vijaivir <91633223+vijaivir@users.noreply.github.com>
2022-05-20 15:42:13 -0700

    Update index.js

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -1,22 +1,33 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
-let myLeads = [];
-const inputBtn = document.getElementById("input-btn");
-const inputEl = document.getElementById("input-el");
-const ulEl = document.getElementById("ul-el");
+localStorage.clear()
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
-inputBtn.addEventListener("click", () => {
-    if(inputEl.value !== ""){
-        myLeads.push(inputEl.value);
-        inputEl.value = "";
-        renderLeads();
+console.log(leadsFromLocalStorage)
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    renderLeads()
+    
+    // To verify that it works:
+    console.log( localStorage.getItem("myLeads") )
+})
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
     }
-});
-
-function renderLeads(){
-    let listItems = "";
-    for(let i = 0; i < myLeads.length; i++){
-        listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
-        console.log(listItems);
-    }
-    ulEl.innerHTML = listItems;
+    ulEl.innerHTML = listItems  
 }
