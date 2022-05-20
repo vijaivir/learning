4ce5b793c262e919b7cfc40b01ada19b7318f0fb
vijaivir <91633223+vijaivir@users.noreply.github.com>
2022-05-20 14:13:56 -0700

    Update index.js
    Added functionality for user input and opening added links in a new tab

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -1,4 +1,22 @@
 
-function saveInput(){
-    console.log("Button clicked!");
+let myLeads = [];
+const inputBtn = document.getElementById("input-btn");
+const inputEl = document.getElementById("input-el");
+const ulEl = document.getElementById("ul-el");
+
+inputBtn.addEventListener("click", () => {
+    if(inputEl.value !== ""){
+        myLeads.push(inputEl.value);
+        inputEl.value = "";
+        renderLeads();
+    }
+});
+
+function renderLeads(){
+    let listItems = "";
+    for(let i = 0; i < myLeads.length; i++){
+        listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+        console.log(listItems);
+    }
+    ulEl.innerHTML = listItems;
 }
