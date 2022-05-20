9dfa7c59b2da8f817e87ab8ceff1f9d80a950679
vijaivir <91633223+vijaivir@users.noreply.github.com>
2022-05-20 13:05:08 -0700

    Update index.js

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -1,22 +1,4 @@
-body {
-    margin: 0;
-    padding: 10px;
-    font-family: Arial, Helvetica, sans-serif;
-}
 
-/*
-
-Style our app according to the provided design!
-
-green color -> #5f9341
-
-*/
-
-
-input {
-    
-}
-
-button {
-    
+function saveInput(){
+    console.log("Button clicked!");
 }
