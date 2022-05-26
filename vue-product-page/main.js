8e23eccbd6dfac6e515761ec47b42cd6f1f3c74f
vijaivir <91633223+vijaivir@users.noreply.github.com>
2022-05-26 12:15:46 -0700

    Update main.js

--- vue-product-page/main.js
+++ vue-product-page/main.js
@@ -1,10 +1,21 @@
+// Passing and options object into the app {}
+// creates a vue instance that's the heart of the app
 const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
             // Solution
-            description: 'A warm fuzzy pair of socks.' 
-            // Solution
+            description: 'A warm fuzzy pair of socks.',
+            //if the image path was changed, the image would be automatically 
+            // updated due to Vue's reactivity system
+            image: './assets/images/socks_green.jpg',
+            url: 'https://google.com',
+            inStock: true, 
+            onSale: true
         }
     }
 })
+
+
+// Vue is reactive
+// built-in Reactivity System (advanced)
